9af868a24c40eab7f052b8288c62c1e58b9e2662
NicolasFrancolino <87394098+NicolasFrancolino@users.noreply.github.com>
2022-05-30 13:10:14 -0300

    Se agregan controladores de vehiculos y ventas

---
+++ ConcecionariaVehiculos/Controllers/VehiculoController.cs
@@ -0,0 +1,64 @@
+﻿using ConcecionariaVehiculos.Entities;
+using ConcecionariaVehiculos.UnitOfWork;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConcecionariaVehiculos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiculoController : ControllerBase
+    {
+        private readonly IUnitOfWork _context;
+
+        public VehiculoController(IUnitOfWork context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Vehiculo>> Get()
+        {
+            var entidad = _context.VehiculoRepo.GetAll();
+            return Ok(entidad);
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Vehiculo vehiculo)
+        {
+            _context.VehiculoRepo.Insert(vehiculo);
+            _context.Save();
+            return Ok();
+        }
+
+
+        [HttpPut("{id}")]
+        public ActionResult Put([FromBody] Vehiculo vehiculo, int id)
+        {
+            try
+            {
+                _context.VehiculoRepo.Update(vehiculo);
+                _context.Save();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var entity = _context.VehiculoRepo.GetById(id);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+            _context.VehiculoRepo.Delete(id);
+            _context.Save();
+
+            return Ok();
+        }
+    }
+}
---
+++ ConcecionariaVehiculos/Controllers/VentaController.cs
@@ -0,0 +1,64 @@
+﻿using ConcecionariaVehiculos.Entities;
+using ConcecionariaVehiculos.UnitOfWork;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConcecionariaVehiculos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VentaController : ControllerBase
+    {
+        private readonly IUnitOfWork _context;
+
+        public VentaController(IUnitOfWork context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Venta>> Get()
+        {
+            var entidad = _context.VentaRepo.GetAll();
+            return Ok(entidad);
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Venta venta)
+        {
+            _context.VentaRepo.Insert(venta);
+            _context.Save();
+            return Ok();
+        }
+
+
+        [HttpPut("{id}")]
+        public ActionResult Put([FromBody] Venta venta, int id)
+        {
+            try
+            {
+                _context.VentaRepo.Update(venta);
+                _context.Save();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var entity = _context.VentaRepo.GetById(id);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+            _context.VentaRepo.Delete(id);
+            _context.Save();
+
+            return Ok();
+        }
+
+    }
+}
